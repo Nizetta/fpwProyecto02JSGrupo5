a35b3a1480322b870d6feecfa12ab658864053f8
AlumnoMA <miguelangelilustrador@gmail.com>
2026-09-10 02:48:53 -0300

    Ejercicio 2 terminado , con la opción del Console.log (para ver en la consola del navegador) o con Alert (para visualizar en el cuador emergente del navegador). Solo una posibilidad  esta habilitada la otra está comentada

---
+++ src/js/ejer2.js
@@ -0,0 +1,5 @@
+let base = (prompt("Ingrese la base del triángulo: "));
+let altura = (prompt("Ingrese la altura del triángulo: "));
+let area = (base * altura) / 2;
+alert("El área del triángulo es: " + area);
+//console.log("El área del triángulo es: " + area);
