501e80170b803499f1b75e0c234bfa88725af4d2
AlumnoMA <miguelangelilustrador@gmail.com>
2026-09-10 02:13:38 -0300

    Resolución de ejercicio 1 y modificaciones a ejer1.html y ejer1.js, ademas agregado de archivos vacios ejer2 a ejer5 en html y js

---
+++ src/js/ejer1.js
@@ -0,0 +1,11 @@
+
+let letra1 = prompt("Ingrese la primera letra: ");
+let letra2 = prompt("Ingrese la segunda letra: ");
+
+/*if (letra1 < letra2) { alert(`La letra ${letra1} es menor que la letra ${letra2}`); }
+else if (letra1 > letra2) { alert(`La letra ${letra1} es mayor que la letra ${letra2}`); }
+else { alert(`Las letras son iguales`); } */
+
+if (letra1 < letra2) { console.log(`La letra ${letra1} es menor que la letra ${letra2}`); }
+else if (letra1 > letra2) { console.log(`La letra ${letra1} es mayor que la letra ${letra2}`); }
+else { console.log(`Las letras son iguales`); }
