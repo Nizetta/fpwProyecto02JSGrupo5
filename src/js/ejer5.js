a9c71b1b4ab6797685c8397eb48fdd788c4470cb
Nizetta <nicolasdenisizetta@gmail.com>
2026-09-14 12:17:35 -0300

    Izetta nicolas borro lineas innecesarias

--- src/js/ejer5.js
+++ src/js/ejer5.js
@@ -10,8 +10,6 @@ formulario.addEventListener("submit",(evento)=>{
     evento.preventDefault();
     const valor1=parseInt(cantPreguntas.value);
     const valor2=parseInt(cantRespuestas.value);
-    // listaDeTareas.push(valor1);
-    // listaDeTareas.push(valor2);
     formulario.reset();
     calcularPorcentaje(valor1,valor2);
 });
