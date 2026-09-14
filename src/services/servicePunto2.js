2dc5dee50d2d23f1f5b5e1dbf9c0b977ad038c9a
castillomartin <castillo.martin.apu@gmail.com>
2026-09-14 10:58:26 -0300

    Desarrollo punto 2

---
+++ src/services/servicePunto2.js
@@ -0,0 +1,7 @@
+export const calcularAreaTriangulo=(base, altura)=>{
+
+    const area = (base * altura)/2;
+    
+    window.alert("La superficie del triángulo con los datos ingresados es: " + area);
+
+};
